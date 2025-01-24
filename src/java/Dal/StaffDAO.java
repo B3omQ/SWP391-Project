298fa4e2df6bd91d417b87b664166a2a7d4dccc2
Long <LongNBHE186897@gmail.com>
2025-01-24 23:44:47 +0700

    Update StaffDAO: add method deleteStaff - Add function delete Staff for CEO

--- src/java/Dal/StaffDAO.java
+++ src/java/Dal/StaffDAO.java
@@ -207,6 +207,18 @@ public class StaffDAO extends DBContext {
             e.printStackTrace();
         }
     }
+    
+    public void deleteStaff(int id) {
+        String sql = "DELETE FROM Staff\n"
+                + "WHERE Id = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, id + "");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+
+        }
+    }
 
     public static void main(String[] args) {
         StaffDAO s = new StaffDAO();
