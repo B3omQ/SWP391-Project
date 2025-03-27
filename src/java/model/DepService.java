e4a1560ec2067ad14617693615ec1291e244c245
Dungpt <phamtiendung508@gmail.com>
2025-03-27 21:02:47 +0700

    Update DepService.java

--- src/java/model/DepService.java
+++ src/java/model/DepService.java
@@ -11,32 +11,18 @@ import java.math.BigDecimal;
  * @author JIGGER
  */
 public class DepService {
-    
+
     private int id;
-    private String depServiceName; 
+    private String depServiceName;
     private String description;
     private BigDecimal minimumDep;
     private int duringTime;
     private double savingRate;
     private double savingRateMinimum;
-    private String reasonReject; 
     private String pendingStatus;
 
     public DepService() {
-    }
 
-    public DepService(int id, String depServiceName, String description, BigDecimal minimumDep, 
-                      int duringTime, double savingRate, double savingRateMinimum, 
-                      String reasonReject, String pendingStatus) {
-        this.id = id;
-        this.depServiceName = depServiceName;
-        this.description = description;
-        this.minimumDep = minimumDep;
-        this.duringTime = duringTime;
-        this.savingRate = savingRate;
-        this.savingRateMinimum = savingRateMinimum;
-        this.reasonReject = reasonReject;
-        this.pendingStatus = pendingStatus;
     }
 
     public DepService(int id, String description, BigDecimal minimumDep, int duringTime, double savingRate) {
@@ -47,14 +33,17 @@ public class DepService {
         this.savingRate = savingRate;
     }
 
-    // Getters và Setters
-    public int getId() {
-        return id;
+    public DepService(int id, String depServiceName, String description, BigDecimal minimumDep, int duringTime, double savingRate, double savingRateMinimum, String pendingStatus) {
+        this.id = id;
+        this.depServiceName = depServiceName;
+        this.description = description;
+        this.minimumDep = minimumDep;
+        this.duringTime = duringTime;
+        this.savingRate = savingRate;
+        this.savingRateMinimum = savingRateMinimum;
+        this.pendingStatus = pendingStatus;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getDepServiceName() {
         return depServiceName;
@@ -64,56 +53,56 @@ public class DepService {
         this.depServiceName = depServiceName;
     }
 
-    public String getDescription() {
-        return description;
+    public int getId() {
+        return id;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public String getDescription() {
+        return description;
     }
 
     public BigDecimal getMinimumDep() {
         return minimumDep;
     }
 
-    public void setMinimumDep(BigDecimal minimumDep) {
-        this.minimumDep = minimumDep;
-    }
-
     public int getDuringTime() {
         return duringTime;
-    }
-
-    public void setDuringTime(int duringTime) {
-        this.duringTime = duringTime;
     }
 
     public double getSavingRate() {
         return savingRate;
     }
 
-    public void setSavingRate(double savingRate) {
-        this.savingRate = savingRate;
-    }
-
     public double getSavingRateMinimum() {
         return savingRateMinimum;
     }
 
-    public void setSavingRateMinimum(double savingRateMinimum) {
-        this.savingRateMinimum = savingRateMinimum;
-    }
-
-    public String getReasonReject() {
-        return reasonReject;
-    }
-
-    public void setReasonReject(String reasonReject) {
-        this.reasonReject = reasonReject;
-    }
-
     public String getPendingStatus() {
         return pendingStatus;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setMinimumDep(BigDecimal minimumDep) {
+        this.minimumDep = minimumDep;
+    }
+
+    public void setDuringTime(int duringTime) {
+        this.duringTime = duringTime;
+    }
+
+    public void setSavingRate(double savingRate) {
+        this.savingRate = savingRate;
+    }
+
+    public void setSavingRateMinimum(double savingRateMinimum) {
+        this.savingRateMinimum = savingRateMinimum;
     }
 
     public void setPendingStatus(String pendingStatus) {
@@ -122,10 +111,7 @@ public class DepService {
 
     @Override
     public String toString() {
-        return "DepService{" + "id=" + id + ", depServiceName=" + depServiceName + 
-               ", description=" + description + ", minimumDep=" + minimumDep + 
-               ", duringTime=" + duringTime + ", savingRate=" + savingRate + 
-               ", savingRateMinimum=" + savingRateMinimum + ", reasonReject=" + reasonReject + 
-               ", pendingStatus=" + pendingStatus + '}';
+        return "DepService{" + "id=" + id + ", depServiceName=" + depServiceName + ", description=" + description + ", minimumDep=" + minimumDep + ", duringTime=" + duringTime + ", savingRate=" + savingRate + ", savingRateMinimum=" + savingRateMinimum + ", pendingStatus=" + pendingStatus + '}';
     }
+
 }
